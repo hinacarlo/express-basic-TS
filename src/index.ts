9221555856841cee5dd73d4d55c4ea70cc4a5744
hinacarlo <hinacarlo1993@gmail.com>
2023-01-19 21:04:46 +0800

    Add routes folder and loginRoutes.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hi there");
+import bodyParser from "body-parser";
+import express, { Request, Response } from "express";
+import { router } from "./routes/loginRoutes";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
+
+app.listen(3000, () => {
+  console.log(`Listening on port 3000`);
+});
