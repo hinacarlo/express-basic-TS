ec11c40cab357c9b57550f81bc00148bbad2febd
hinacarlo <hinacarlo1993@gmail.com>
2023-01-22 21:12:06 +0800

    Add protected route and add a requireAuth function as middleware

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,8 +1,18 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 
 interface ReqWithBody extends Request {
   body: { [key: string]: string | undefined };
 }
+
+const requireAuth = (req: Request, res: Response, next: NextFunction): void => {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send("Not permitted");
+};
 
 const router = Router();
 
@@ -56,6 +66,15 @@ router.get("/", (req: Request, res: Response) => {
       </div>
     `);
   }
+});
+
+router.get("/logout", (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect("/");
+});
+
+router.get("/protected", requireAuth, (req: Request, res: Response) => {
+  res.send("Welcome to protected route, logged in user");
 });
 
 export { router };
