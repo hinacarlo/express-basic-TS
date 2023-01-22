ffcf76bc2bbf52c81bd5c8a95a933e71981041c0
hinacarlo <hinacarlo1993@gmail.com>
2023-01-22 20:57:22 +0800

    Add cookie session for logged in users

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,5 +1,9 @@
 import { Request, Response, Router } from "express";
 
+interface ReqWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
 const router = Router();
 
 router.get("/login", (req: Request, res: Response) => {
@@ -18,13 +22,39 @@ router.get("/login", (req: Request, res: Response) => {
   `);
 });
 
-router.post("/login", (req: Request, res: Response) => {
+router.post("/login", (req: ReqWithBody, res: Response) => {
   const { email, password } = req.body;
 
-  if (email) {
-    res.send(email.toUpperCase() + password);
+  if (
+    email &&
+    password &&
+    email === "carlo@carlo.com" &&
+    password === "password"
+  ) {
+    // mark this person as logged in
+    req.session = { loggedIn: true };
+    // redirect them to the root route
+    res.redirect("/");
   } else {
-    res.send("Provide email");
+    res.send("Provide valid email or password");
+  }
+});
+
+router.get("/", (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+  } else {
+    res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href="/login">Login</a>
+      </div>
+    `);
   }
 });
 
